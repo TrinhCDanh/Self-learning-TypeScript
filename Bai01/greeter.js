ec135551e84adc6ebb45017030502683300d1fe3
TrinhCDanh <trinhdanh04@gmail.com>
2018-02-10 21:17:27 +0700

    Bai01

---
+++ Bai01/greeter.js
@@ -0,0 +1,5 @@
+function greeter(person) {
+    return "Hello, " + person;
+}
+var user = "ES6";
+document.body.innerHTML = greeter(user);
---
+++ Bai01/ts/greeter.ts
@@ -0,0 +1,7 @@
+function greeter(person: string) {
+	return "Hello, " + person;
+}
+
+var user = "ES6";
+
+document.body.innerHTML = greeter(user);

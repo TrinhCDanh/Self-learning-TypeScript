77305f8ad90f0b4f97886a350af01e97513f08e7
TrinhCDanh <trinhdanh04@gmail.com>
2018-02-10 22:36:19 +0700

    variable any

---
+++ Bai01/js/03_variable/05-any.js
@@ -0,0 +1,6 @@
+// chưa xác định biến mang kiểu dữ liệu gì thì khai báo biến theo kiểu any
+let mVariable = 1;
+mVariable = "abc";
+let mArray = [1, true, "abc"];
+console.log(mVariable);
+console.log(mArray);
---
+++ Bai01/ts/03_variable/05-any.ts
@@ -0,0 +1,9 @@
+// chưa xác định biến mang kiểu dữ liệu gì thì khai báo biến theo kiểu any
+let mVariable: any = 1;
+mVariable = "abc";
+
+
+let mArray: any[] = [1, true, "abc"];
+
+console.log(mVariable);
+console.log(mArray);
